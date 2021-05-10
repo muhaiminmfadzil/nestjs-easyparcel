4529c99464dd5f13dc3f891463dd266c70709d8a
Muhaimin <muhaiminmfadzil@gmail.com>
2021-05-10 15:47:16 +0800

    service

--- src/easyparcel/easyparcel.service.ts
+++ src/easyparcel/easyparcel.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Inject, Injectable, LoggerService } from '@nestjs/common';
+import { CONFIG_OPTIONS, EasyparcelOptions } from './easyparcel.definition';
 
 @Injectable()
-export class EasyparcelService {}
+export class EasyparcelService {
+  sandbox: boolean;
+  private apiKey: string;
+  private logService?: LoggerService;
+  private demoUrl: 'https://demo.connect.easyparcel.my/?ac=';
+  private liveUrl: 'https://connect.easyparcel.my/?ac=';
+  private baseUrl: string;
+
+  constructor(
+    private httpService: HttpService,
+    @Inject(CONFIG_OPTIONS) options: EasyparcelOptions,
+  ) {
+    this.apiKey = options.apiKey;
+    this.sandbox = options.sandbox || false;
+    this.apiKey = options.apiKey;
+    this.logService = options.logger;
+    this.setBaseUrl();
+  }
+
+  setBaseUrl() {
+    if (this.sandbox === false) {
+      this.baseUrl = this.liveUrl;
+    } else {
+      this.baseUrl = this.demoUrl;
+    }
+  }
+}
